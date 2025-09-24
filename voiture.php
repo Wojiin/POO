924e3a138ce3ed721a2be32aa21351ad2b6129e7
Quentin Maia <q.maia@laposte.net>
2025-09-24 16:04:24 +0200

    Update voiture.php

--- voiture.php
+++ voiture.php
@@ -1,6 +1,6 @@
 <?php
-/*L'objet est la construction basée sur la classe. Il contient les
-        attributs de la classe mais spécifiée pour lui même.
+/*L'objet est la construction basée sur la classe (instance de la classe). Il contient les
+    attributs de la classe mais spécifiée pour lui même.
 
 La classe définie un modèle pour les objets lui appartenant,
     comme un plan de construction général et sans spécifications
@@ -11,46 +11,56 @@ Class Voiture{
     private $_modele;
     private $_nbPortes;
     private $_vitesseActuelle;
+
+    //Le Getter est une méthode publique dans une classe qui permet de lire la valeur d'un attribut privé ou protégé sans le modifier.
+    public function getMarque() {
+        return $this->_marque;
+    }
+    //Le Setter est une méthode qui permet de modifier la valeur d'un attribut privé ou protégé. 
+    public function setMarque($marque) {
+    $this->_marque = $marque;
+}   
+
 /*la méthode permet de mettre l'objet en action en utilisant des fonctions
     propres à l'objet  */    
     public function demarrer(){
-        $this->statut
     }
-        public function accelerer(){
-        $this->statut
+        public function accelerer(){  
     }
         public function stopper(){
-        $this->statut
     }
-        /* Méthode toString reprend les attributs et les renvois
-            sous forme de string*/
+
+/* Méthode toString reprend les attributs et les renvois
+     sous forme de string*/
     public function __toString() {
-        return "Marque: {$this->$_marque}, 
-                Modèle: {$this->$_modele}, 
-                Nombre de portes: {$this->$_nbPortes}, 
-                Vitesse actuelle: {$this->$_vitesseActuelle}";
+        return "Marque: {$this->_marque}, 
+                Modèle: {$this->_modele}, 
+                Nombre de portes: {$this->_nbPortes}, 
+                Vitesse actuelle: {$this->_vitesseActuelle}";
     }
-}
+
 
 /*Le Constructeur est l'artisan de l'objet, il se base sur la classe et 
     initialise les attributs*/
     
-Class Voiture{
-       //La fonction magique initialise les attributs de l'objet
-    public function __construct($marque, $modele, $nbPortes, $_vitesseActuelle){
+    //La fonction magique initialise les attributs de l'objet
+    public function __construct($marque, $modele, $nbPortes, $vitesseActuelle){
         $this->_marque = $marque;             
         $this->_modele = $modele;              
         $this->_nbPortes = $nbPortes;          
         $this->_vitesseActuelle = $vitesseActuelle;
     }
-}   
-
-// Création de l'objet Voiture qui spécifie les attributs
-$Voiture1 = new Voiture("Peugeot","206","2","0");
-$Voiture2 = new Voiture("Peugeot","308","5","0");
+}
+//Création de l'objet Voiture qui spécifie les attributs
+$voiture1 = new Voiture("Peugeot","206",2,0);
+$voiture2 = new Voiture("Peugeot","308",5,0);
 
 /*L'Encapsulation permet de rendre plus ou moins accessibles les attributs et 
     les méthodes de l'objet notamment dans le but 
-    d'en limiter les modifications qui le dénaturent enleur donnant un niveau d'accès.
-    Il y en a 3 "public","private","protected"*/
+    d'en limiter les modifications qui le dénaturent enleur donnant un niveau d'accès, généralement par des méthodes publiques "getter" et "setter".
+    Il y en a 3 :
+        public : accessible partout
+        private : accessible uniquement dans la classe
+        protected : accessible dans la classe et ses sous-classes (héritage)*/
+
 
